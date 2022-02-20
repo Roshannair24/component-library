6c3b6d70f7190d61f81b8650412f5dabe79c2eb2
Roshannair24 <70207465+Roshannair24@users.noreply.github.com>
2022-02-21 02:01:39 +0530

    styles: added responsiveness
    added responsiveness

---
+++ index.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", reportWindowSize);
+window.addEventListener("resize", reportWindowSize);
+
+function reportWindowSize() {
+  if (window.innerWidth <= 500) {
+    // document.querySelector("iframe").style.width = "300px";
+
+    let allIframes = document.querySelectorAll("iframe");
+
+    allIframes.forEach((currentValue, index, arr) => {
+      currentValue.style.width = "300px";
+    });
+  }
+}
