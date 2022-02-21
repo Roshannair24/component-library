8cc2160ccecd614c93b791e4f47cae80f0df2e55
Roshannair24 <70207465+Roshannair24@users.noreply.github.com>
2022-02-21 23:44:11 +0530

    feat: added darkmode
    added darkmode

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function reportWindowSize() {
   if (window.innerWidth <= 500) {
     // document.querySelector("iframe").style.width = "300px";
 
-    
     document.querySelector("#homepage-iframe").style.width = "300px";
 
     let allIframes = document.querySelectorAll("iframe");
@@ -14,4 +13,48 @@ function reportWindowSize() {
       currentValue.style.width = "300px";
     });
   }
+}
+
+let darkBtn = document.querySelector("#nav-dark-mode-btn");
+
+darkBtn.addEventListener("click", () => applyDarkMode);
+
+darkBtn.addEventListener("click", function () {
+  applyDarkMode();
+});
+
+function applyDarkMode() {
+  document.querySelector(".bxs-sun")?.classList.toggle("hide-dark-icon");
+  document.querySelector(".bxs-moon")?.classList.toggle("hide-dark-icon");
+
+  document.querySelector(".master")?.classList.toggle("dark-background");
+  document.querySelector(".all-content")?.classList.toggle("light-foreground");
+  document.querySelector(".k-footer")?.classList.toggle("dark-background");
+
+  document
+    .querySelector(".wallpaper-base")
+    ?.classList.toggle("dark-background");
+  document
+    .querySelector(".installation-guide-container")
+    ?.classList.toggle("dark-background");
+
+  document
+    .querySelector(".installation-guide-text")
+    ?.classList.toggle("dark-background");
+
+  // document.querySelector("#list-iframe-v-container-leftside").classList.toggle("light-background");
+
+  let sidebarBtnArr = document.querySelectorAll(".k-link");
+
+  let notifArr = document.querySelectorAll(".list-iframe-v-container-leftside");
+
+  sidebarBtnArr.forEach((currentValue, index, arr) => {
+    currentValue.classList.toggle("light-foreground");
+  });
+
+  notifArr.forEach((currentValue, index, arr) => {
+    currentValue.classList.toggle("light-background");
+  });
+
+  // document.querySelectorAll(".k-link").classList.toggle("light-foreground");
 }
